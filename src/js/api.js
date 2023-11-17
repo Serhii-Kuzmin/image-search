04424757216a3db95cef8a519bdbf6dbf0c6b1df
Serhii-Kuzmin <seryy8248@gmail.com>
2023-11-17 23:59:13 +0100

    commit-homework-js

---
+++ src/js/api.js
@@ -0,0 +1,16 @@
+export const BASE_URL = 'https://pixabay.com/api/';
+export const API_KEY = '40717223-2bc60fe276e82255cc8c662af';
+export const options = {
+    params: {
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+    page: 1,
+    q: '',
+  },
+}
+
+
+  
